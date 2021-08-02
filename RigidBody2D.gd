f84869b1f1b0bcbe2d9517ade7bf7d5025678135
Masashidoo <liondoria22@gmail.com>
2021-08-02 20:24:25 -0300

    sdad
    asd

---
+++ RigidBody2D.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+var vector = Vector2.ZERO;
+
+func _physics_process(delta):
+	
+	vector.x = Input.get_action_strength("d") - Input.get_action_strength("a");
+	vector.y = Input.get_action_strength("s") - Input.get_action_strength("w");
+	
+	vector = vector.normalized();
+	
+	if vector.length() > 0:
+		add_force(Vector2.ZERO, vector * delta);
