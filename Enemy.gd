6da493f022600a0e827eb13d215df45e1889ffc8
ZippyNio <40307374+NioZX@users.noreply.github.com>
2021-08-02 22:07:41 -0300

    Bugs in enemy

--- Enemy.gd
+++ Enemy.gd
@@ -9,6 +9,7 @@ var player_last_pos
 var player_pos = Vector2(50,80)
 var can_jump = true
 var speed = 50
+var enemy_last_pos = self.position
 
 var path_index = 0;
 
@@ -20,30 +21,36 @@ func _ready():
 
 
 func get_path():
-	current_path = $Navigation2D.get_simple_path(self.position,player_pos)
+	current_path = $Navigation2D.get_simple_path(get_global_position(),player_pos)
+	current_path[1] = transform.xform(current_path[1])
 	$Line2D.points = current_path
 
 func _input(event):
    # Mouse in viewport coordinates.
-   if event is InputEventMouseMotion:
-	   player_pos = event.position
+	if event is InputEventMouseButton:
+		player_pos = event.position
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#$Navigation2D.position = Vector2.ZERO
+	#$Navigation2D/NavigationPolygonInstance.position = Vector2.ZERO
+	
 	if (player_pos != player_last_pos):
-		var start_pos = current_path[0]
-		var end_pos = current_path[1]
+		var start_pos = self.position
+		var end_pos = $Navigation2D.get_closest_point(player_pos)
+		print(end_pos)
 		
 		var distance_to_path = start_pos.distance_to(end_pos) 
 		var current_distance = self.position.distance_to(end_pos) 
 		var path_completed = (current_distance - 0) / (distance_to_path - 0)
-		var speed_to_path = start_pos.linear_interpolate(end_pos, path_completed)
+		var speed_to_path = start_pos.linear_interpolate(end_pos, path_completed) * ( end_pos - self.position ).normalized()
 		
-		print(path_completed)
 		if (path_completed < 0.05):
-			get_path()
 			player_last_pos = player_pos
 		
+		
 		move_and_slide(speed_to_path)
+
 		
 	
