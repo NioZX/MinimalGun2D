2fde3483fc30aa3f0f5a5974410dfb19026ec4a8
ZippyNio <40307374+NioZX@users.noreply.github.com>
2021-08-02 21:16:02 -0300

    Enemy AI testing

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -7,7 +7,7 @@ func _physics_process(delta):
 	vector.x = Input.get_action_strength("d") - Input.get_action_strength("a");
 	vector.y = Input.get_action_strength("s") - Input.get_action_strength("w");
 	
-	vector = vector.normalized();
+	vector = vector.normalized() * 20;
 	
 	if vector.length() > 0:
 		add_force(Vector2.ZERO, vector * delta);
